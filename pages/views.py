52b62b2497a87b2780d4eb558d66c2b261ecd8b3
todd427 <todd427@gmail.com>
2025-06-16 21:36:05 -0700

    worked on /test-email/ backend

--- pages/views.py
+++ pages/views.py
@@ -18,20 +18,24 @@ from django.views import View
 class TestTemplateView(View):
     def get(self, request):
         return render(request, 'pages/test.html')
+    
 from django.core.mail import send_mail
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 @login_required
 def test_email_view(request):
     try:
         send_mail(
-            subject="Mork lives!",
-            message="🎉 Trying this again.",
+            subject="test-email.txt",
+            message = render_to_string("emails/test-email.txt"),
             from_email="mork@foxxelabs.com",
             recipient_list=["todd@toddwriter.com"],
             fail_silently=False,
         )
-        return HttpResponse("✅ Email sent successfully via SMTP2GO.")
+        return HttpResponse(f"✅ Email sent successfully using backend: {settings.EMAIL_BACKEND}")
+
     except Exception as e:
         return HttpResponse(f"❌ Email failed: {str(e)}", status=500)
