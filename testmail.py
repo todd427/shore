0624d5c8db2bd93f152768c31962e96222390f55
todd427 <todd427@gmail.com>
2025-06-16 18:39:10 -0700

    removed testmail.py

--- testmail.py
+++
@@ -1,9 +0,0 @@
-from django.core.mail import send_mail
-
-send_mail(
-    subject="SMTP2GO Test Email",
-    message="This is a test email sent using SMTP2GO with Django.",
-    from_email="foxxelab@foxxelabs.com",  # Must match SMTP2GO verified sender or domain
-    recipient_list=["todd@toddwriter.com"],  # Replace with your destination email
-    fail_silently=False,
-)
